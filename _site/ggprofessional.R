6c8bc05a73ea1b2b36b948b3d91c394822f71e68
MarcellGranat <granatcellmar98@gmail.com>
2023-04-24 10:19:39 +0200

    statics

---
+++ _site/ggprofessional.R
@@ -0,0 +1,3 @@
+setwd("/Users/marci/projects/marcellgranat-website/static/ggprofessional")
+pkgdown::build_site_github_pages(pkg = "~/projects/ggProfessional", dest_dir = getwd())
+
---
+++ _site/granatlib.R
@@ -0,0 +1,2 @@
+setwd("/Users/marci/projects/marcellgranat-website/static/granatlib")
+pkgdown::build_site_github_pages(pkg = "~/projects/granatlib", dest_dir = getwd())
